6d810d1b0c4ee3e16ff56315dc3f0fba55372e52
KoSakaki <sakaki@flaginc.net>
2022-09-22 13:32:21 +0900

    create api

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
-	"gotrading/config"
 	"gotrading/app/models"
+	"gotrading/config"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"text/template"
 )
 
@@ -22,7 +26,61 @@ func viewChartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type JSONError struct {
+	Error string `json:"error"`
+	Code  int    `json:"code"`
+}
+
+func APIError(w http.ResponseWriter, errMessage string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	jsonError, err := json.Marshal(JSONError{Error: errMessage, Code: code})
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonError)
+}
+
+var apiValidPath = regexp.MustCompile("^/api/candole/$")
+
+func apiMakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := apiValidPath.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			APIError(w, "Not Found", http.StatusNotFound)
+		}
+		fn(w, r)
+	}
+}
+
+func apiCandoleHandler(w http.ResponseWriter, r *http.Request) {
+	productCode := r.URL.Query().Get("product_code")
+	if productCode == "" {
+		APIError(w, "No product_code", http.StatusBadRequest)
+		return
+	}
+	strlimit := r.URL.Query().Get("limit")
+	limit, err := strconv.Atoi(strlimit)
+	if strlimit == "" || err != nil || limit < 0 || limit > 1000{
+		limit = 1000
+	}
+	duration := r.URL.Query().Get("duration")
+	if duration == "" {
+		duration = "1m"
+	}
+	durationTime := config.Config.Durations[duration]
+
+	df, _ := models.GetAllCandole(productCode, durationTime, limit)
+	js, err := json.Marshal(df)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
 func StartWebServer() error {
+	http.HandleFunc("/api/candole/", apiMakeHandler(apiCandoleHandler))
 	http.HandleFunc("/chart/", viewChartHandler)
 	return http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil)
 }
